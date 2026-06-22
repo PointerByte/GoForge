2b74f0c6b65d812f3f847555db73998c8ffeee02
Manuel Garcia Blanco <many_mgb_21@hotmail.com>
2026-06-21 23:42:15 -0600

    chore: update default GoForge and logger versions to v0.1.10 and v0.0.47 respectively

--- cmd/qgo/code/scaffold.go
+++ cmd/qgo/code/scaffold.go
@@ -116,8 +116,8 @@ func promptRequired(reader *bufio.Reader, output io.Writer, label string, fallba
 }
 
 const (
-	defaultGoForgeVersion       = "v0.1.9"
-	defaultGoForgeLoggerVersion = "v0.0.46"
+	defaultGoForgeVersion       = "v0.1.10"
+	defaultGoForgeLoggerVersion = "v0.0.47"
 )
 
 // promptConfigFormat resolves the config format, defaulting to YAML when the user leaves it blank.
